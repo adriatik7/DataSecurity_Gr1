e4ace9551a5017c485eb2a33aed811898cd98ba5
Armela Hajra <48591872+armelahajra@users.noreply.github.com>
2023-04-16 13:42:29 +0200

    Update MorseCode.py

--- MorseCode.py
+++ MorseCode.py
@@ -3,8 +3,15 @@ import time
 
 morse_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
 
-
-
+def encode_morse_code(text):
+    text = text.upper()
+    encoded_text = []
+    for char in text:
+        if char == ' ':
+            encoded_text.append('/')
+        else:
+            encoded_text.append(morse_code_dict[char])
+    return ' '.join(encoded_text)
 
 def decode_morse_code(encoded_text):
     encoded_text = encoded_text.split(' ')
